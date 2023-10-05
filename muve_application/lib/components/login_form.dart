ba81339a3a98743c9759a74bcabdc5483cb4eb2a
maxhayes17 <52089291+maxhayes17@users.noreply.github.com>
2023-10-04 21:24:16 -0400

    Start login and loginForm

---
+++ muve_application/lib/components/login_form.dart
@@ -0,0 +1,14 @@
+// import 'package:flutter/material.dart';
+
+// class LoginForm extends StatelessWidget{
+//   const LoginForm({super.key});
+
+//   @override
+//   Widget build(BuildContext context){
+//     return Padding(
+//       padding: const EdgeInsets.symmetric(vertical: 50.0)
+//       child: const TextField(),
+//       );
+//   }
+
+// }
