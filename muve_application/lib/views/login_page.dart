3f97adff9096fa141b1b6f12cec994bb1dc7dbdf
jeremyB2 <99774165+jeremyB2@users.noreply.github.com>
2023-11-06 16:52:34 -0500

    basic dart housekeeping

--- muve_application/lib/views/login_page.dart
+++ muve_application/lib/views/login_page.dart
@@ -7,50 +7,38 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-          children: <Widget>[
-            SizedBox(
-              height: 150,
+        body: Column(children: <Widget>[
+      const SizedBox(
+        height: 150,
+      ),
+      const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 50),
+          child: FittedBox(
+            child: Text(
+              "Motivational Message",
+              style: TextStyle(
+                fontSize: 60,
+                fontWeight: FontWeight.w800,
+              ),
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 50),
-              child: FittedBox(
-                child: Text(
-                  "Motivational Message",
-                  style: TextStyle(
-                    fontSize: 60,
-                    fontWeight: FontWeight.w800,
-                  ),
-                ),
-              )
-            ),
-            Expanded(
-              // Align card to bottom-center
-              child: Align(
-                alignment: FractionalOffset.bottomCenter,
-                  // Create rounded corners
-                  child: ClipRRect(
-                    borderRadius: const BorderRadius.only(
-                        topLeft: Radius.circular(30), 
-                        topRight: Radius.circular(30)
-                    ),
-                    // Card itself
-                    child: Container(
-                      height: MediaQuery.of(context).size.height * (0.6),
-                      width: MediaQuery.of(context).size.width,
-                      color: Colors.blueGrey,
-                      child: Padding(
-                        padding: EdgeInsets.all(50),
-                        child: LoginForm()
-                        ),
-                      )
-                    ),
-                  )
-            )
-        ]
-      )
-    );
+          )),
+      Expanded(
+          // Align card to bottom-center
+          child: Align(
+        alignment: FractionalOffset.bottomCenter,
+        // Create rounded corners
+        child: ClipRRect(
+            borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(30), topRight: Radius.circular(30)),
+            // Card itself
+            child: Container(
+              height: MediaQuery.of(context).size.height * (0.6),
+              width: MediaQuery.of(context).size.width,
+              color: Colors.blueGrey,
+              child: const Padding(
+                  padding: EdgeInsets.all(50), child: LoginForm()),
+            )),
+      ))
+    ]));
   }
-
 }
-
