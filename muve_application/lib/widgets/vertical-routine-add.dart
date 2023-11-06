e9600a4d777700c42f98c8c93116d81961c8768f
maxhayes17 <52089291+maxhayes17@users.noreply.github.com>
2023-11-06 11:28:21 -0500

    Create add routine button, library page

---
+++ muve_application/lib/widgets/vertical-routine-add.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:muve_application/routes.dart' as routes;
+import 'package:muve_application/models/routine_model.dart';
+
+class VerticalRoutineAdd extends StatelessWidget {
+
+const VerticalRoutineAdd({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Container(
+        padding: EdgeInsets.only(top: 10, bottom: 10),
+        child: Row(
+          children: [
+            Container(width: MediaQuery.of(context).size.height / 12, 
+            height: MediaQuery.of(context).size.height / 12, 
+            color: Colors.blueGrey,
+            ),
+            SizedBox(width: 10),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start, // Align text to the left
+              children: [
+                Text('Add routine...', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+              ],
+            )
+          ],
+
+        ),
+      ),
+      onTap: () => context.push(routes.composePath)
+      );
+  }
+}
