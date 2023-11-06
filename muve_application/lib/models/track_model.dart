de8bd30cccfdd59d125fa7f4010e91997ff50c0c
jeremyB2 <99774165+jeremyB2@users.noreply.github.com>
2023-11-06 16:45:25 -0500

    the rest of last commit

--- muve_application/lib/models/track_model.dart
+++ muve_application/lib/models/track_model.dart
@@ -1,27 +1,23 @@
 class Track {
-
   final String id;
   final String name;
   final String artist;
   final String duration;
-  final String picture_path;
+  final String picturePath;
 
-  Track({
-    required this.id,
-    required this.name,
-    required this.artist,
-    required this.duration,
-    required this.picture_path
-  });
+  Track(
+      {required this.id,
+      required this.name,
+      required this.artist,
+      required this.duration,
+      required this.picturePath});
 
-  factory Track.fromJson(Map<String, dynamic> json){
+  factory Track.fromJson(Map<String, dynamic> json) {
     return Track(
-      id: json['id'],
-      name: json['name'],
-      artist: json['artist'],
-      duration: json['duration'],
-      picture_path: json['picture_path']
-    );
+        id: json['id'],
+        name: json['name'],
+        artist: json['artist'],
+        duration: json['duration'],
+        picturePath: json['picture_path']);
   }
-
 }
--- muve_application/lib/views/home_page.dart
+++ muve_application/lib/views/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:muve_application/viewmodels/routine_view_model.dart';
 import 'package:muve_application/viewmodels/user_view_model.dart';
 import 'package:muve_application/widgets/vertical_routine.dart';
 import 'package:provider/provider.dart';
@@ -9,24 +8,27 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final routineVM = context.read<RoutineViewModel>();
     final userVM = context.read<UserViewModel>();
 
     return SafeArea(
-      child: Padding(
-        padding: EdgeInsets.only(left: 32, right: 32),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start, // Align text to the left
+        child: Padding(
+      padding: const EdgeInsets.only(left: 32, right: 32),
+      child: Column(
+          crossAxisAlignment:
+              CrossAxisAlignment.start, // Align text to the left
           children: [
-            SizedBox(height: 24,),
-            const Text("Recent Routines", style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
-            SizedBox(height: 12,),
+            const SizedBox(
+              height: 24,
+            ),
+            const Text("Recent Routines",
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
+            const SizedBox(
+              height: 12,
+            ),
             Expanded(
               child: VerticalRoutine(routines: userVM.getRecentRoutines()),
-              )
-          ]
-        ),
-      )
-    );
+            )
+          ]),
+    ));
   }
 }
