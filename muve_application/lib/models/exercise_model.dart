4462addb18028ad7cf9de5512f2a70a2aaba91db
maxhayes17 <52089291+maxhayes17@users.noreply.github.com>
2023-11-10 19:02:42 -0500

    Make exercise name optional

--- muve_application/lib/models/exercise_model.dart
+++ muve_application/lib/models/exercise_model.dart
@@ -2,11 +2,11 @@ import "package:muve_application/models/set_model.dart";
 
 class Exercise {
   final int id;
-  final String name;
+  final String? name;
   final String? notes;
   final List<ExerciseSet>? sets;
 
-  Exercise({required this.id, required this.name, this.notes, this.sets});
+  Exercise({required this.id, this.name, this.notes, this.sets});
 
   factory Exercise.fromJson(Map<String, dynamic> json) {
     return Exercise(
