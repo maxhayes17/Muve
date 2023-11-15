56ee8c37a33d832d9f526b1ae86f03e3a4f0e035
maxhayes17 <52089291+maxhayes17@users.noreply.github.com>
2023-11-14 19:36:04 -0500

    Move fake data to data.dart file

--- muve_application/lib/viewmodels/routine_view_model.dart
+++ muve_application/lib/viewmodels/routine_view_model.dart
@@ -4,20 +4,19 @@ import "package:muve_application/models/routine_model.dart";
 import "package:muve_application/models/set_model.dart";
 import "package:muve_application/models/track_model.dart";
 
+// FAKE DATA
+import "package:muve_application/data.dart";
+
 class RoutineViewModel with ChangeNotifier {
-  final _routineList = routineDatabase;
 
   Routine? getRoutineById(int id) {
-    for (var routines in _routineList.values) {
-      for (var routine in routines) {
-        if (routine.id == id) {
-          return routine;
-        }
+    for (var routine in routines) {
+      if (routine.id == id) {
+        return routine;
       }
     }
     return null;
   }
-
 
   // String get name => this.name;
   // String get duration => this.duration;
@@ -27,56 +26,3 @@ class RoutineViewModel with ChangeNotifier {
   // List<Exercise> get exercises => this.exercises;
   // String get picture_path => this.picture_path;
 }
-
-final List<Exercise> _exercises = [
-    Exercise(id: 0, name: 'Bench press', sets: [ExerciseSet(id: 1, weight: 50, repetitions: 3), ExerciseSet(id: 2, weight: 60, repetitions: 3), ExerciseSet(id: 3, weight: 70, repetitions: 3)]),
-    Exercise(id: 0, name: 'Curl', sets: [ExerciseSet(id: 1, weight: 50, repetitions: 3), ExerciseSet(id: 2, weight: 60, repetitions: 3)]),
-];
-
-final List<Track> _tracks = [
-  Track(id: 1, name: 'name', artist: 'artist', duration: 'duration', picturePath: 'picturePath'),
-  Track(id: 2, name: 'name', artist: 'artist', duration: 'duration', picturePath: 'picturePath'),
-  Track(id: 3, name: 'name', artist: 'artist', duration: 'duration', picturePath: 'picturePath'),
-  Track(id: 4, name: 'name', artist: 'artist', duration: 'duration', picturePath: 'picturePath'),
-  Track(id: 5, name: 'name', artist: 'artist', duration: 'duration', picturePath: 'picturePath'),
-  Track(id: 6, name: 'name', artist: 'artist', duration: 'duration', picturePath: 'picturePath')
-];
-
-final Map<int, List<Routine>> routineDatabase = {
-  1: jeremyRoutineList,
-  2: maxRoutineList,
-  3: ethanRoutineList,
-};
-
-final List<Routine> jeremyRoutineList = [
-  Routine(id: 1, name: "j_routine1", duration: "30 min", author: "jeremy", exercises: _exercises, tracks: _tracks),
-  Routine(id: 2, name: "j_routine2", duration: "1 hour", author: "jeremy", exercises: _exercises, tracks: []),
-  Routine(id: 3, name: "j_routine3", duration: "45 min", author: "jeremy", exercises: _exercises, tracks: []),
-  Routine(id: 4, name: "j_routine4", duration: "1 hour", author: "jeremy", exercises: _exercises, tracks: []),
-  Routine(id: 5, name: "j_routine5", duration: "1.5 hour", author: "jeremy", exercises: _exercises, tracks: []),
-  Routine(id: 6, name: "j_routine6", duration: "20 min", author: "jeremy", exercises: _exercises, tracks: []),
-  Routine(id: 7, name: "j_routine7", duration: "15 hour", author: "jeremy", exercises: _exercises, tracks: []),
-  Routine(id: 8, name: "j_routine8", duration: "1 hour", author: "jeremy", exercises: _exercises, tracks: []),
-  Routine(id: 9, name: "j_routine9", duration: "25 min", author: "jeremy", exercises: _exercises, tracks: []),
-];
-
-final List<Routine> maxRoutineList = [
-  Routine(id: 10, name: "m_routine1", duration: "30 min", author: "max", exercises: _exercises, tracks: _tracks),
-  Routine(id: 11, name: "m_routine2", duration: "1 hour", author: "max", exercises: _exercises, tracks: []),
-  Routine(id: 12, name: "m_routine3", duration: "45 min", author: "max", exercises: _exercises, tracks: []),
-  Routine(id: 13, name: "m_routine4", duration: "1 hour", author: "max", exercises: _exercises, tracks: []),
-  Routine(id: 14, name: "m_routine5", duration: "1.5 hour", author: "max", exercises: _exercises, tracks: []),
-  Routine(id: 15, name: "m_routine6", duration: "20 min", author: "max", exercises: _exercises, tracks: []),
-  Routine(id: 16, name: "m_routine7", duration: "15 hour", author: "max", exercises: _exercises, tracks: []),
-  Routine(id: 17, name: "m_routine8", duration: "1 hour", author: "max", exercises: _exercises, tracks: []),
-  Routine(id: 18, name: "m_routine9", duration: "25 min", author: "max", exercises: _exercises, tracks: []),
-];
-
-final List<Routine> ethanRoutineList = [
-  Routine(id: 19, name: "e_routine1", duration: "1 hour", author: "ethan", exercises: _exercises, tracks: []),
-  Routine(id: 20, name: "e_routine2", duration: "1.5 hour", author: "ethan", exercises: _exercises, tracks: []),
-  Routine(id: 21, name: "e_routine3", duration: "20 min", author: "ethan", exercises: _exercises, tracks: []),
-  Routine(id: 22, name: "e_routine4", duration: "15 hour", author: "ethan", exercises: _exercises, tracks: []),
-  Routine(id: 23, name: "e_routine5", duration: "1 hour", author: "ethan", exercises: _exercises, tracks: []),
-  Routine(id: 24, name: "e_routine6", duration: "25 min", author: "ethan", exercises: _exercises, tracks: []),
-];
--- muve_application/lib/viewmodels/user_view_model.dart
+++ muve_application/lib/viewmodels/user_view_model.dart
@@ -3,8 +3,10 @@ import "package:muve_application/models/routine_model.dart";
 import "package:muve_application/models/user_model.dart";
 import "package:muve_application/viewmodels/routine_view_model.dart";
 
+// FAKE DATA
+import "package:muve_application/data.dart";
+
 class UserViewModel with ChangeNotifier {
-  final Map<String, User> _userList = _userDatabase;
   User? _user;
 
   User? get user => _user;
@@ -14,13 +16,14 @@ class UserViewModel with ChangeNotifier {
   List<Routine>? get routines => _user?.routines;
 
   bool authenticateUser(String email, String password) {
-    if (_userList[email]?.password == password) {
-      _user = _userList[email];
-      notifyListeners();
-      return true;
-    } else {
-      return false;
+    for (var user in users){
+      if (user.email == email && user.password == password) {
+        _user = user;
+        notifyListeners();
+        return true;
+      }
     }
+    return false;
   }
    // Assuming last 3 added routines are most recent... return last 3 routines
   List<Routine> getRecentRoutines(){
@@ -32,14 +35,4 @@ class UserViewModel with ChangeNotifier {
     }
   }
 
-
 }
-
-final Map<String, User> _userDatabase = {
-  "jeremy@muve.com":
-      User(id: 1, email: "jeremy@muve.com", username: "jeremy", password: "pw", routines: jeremyRoutineList),
-  "max@muve.com":
-      User(id: 2, email: "max@muve.com", username: "max", password: "pw", routines: maxRoutineList),
-  "ethan@muve.com":
-      User(id: 3, email: "ethan@muve.com", username: "ethan", password: "pw", routines: ethanRoutineList)
-};
