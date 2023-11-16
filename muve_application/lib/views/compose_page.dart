8dde82cc4eaaf4f766aa954418210a58dd727d9f
maxhayes17 <52089291+maxhayes17@users.noreply.github.com>
2023-11-16 14:17:55 -0500

    Center buttons on compose page

--- muve_application/lib/views/compose_page.dart
+++ muve_application/lib/views/compose_page.dart
@@ -66,45 +66,52 @@ class ComposePage extends StatelessWidget {
                 ),
             //),
             SizedBox(height: 128,),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.8,
-              height: 50,
-              child: FilledButton(
-                style: FilledButton.styleFrom(backgroundColor: Colors.blueGrey[700]),
-                onPressed: () => context.push(routes.addExercisePath),
-                child: const Text(
-                  "Add Exercises",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                ),
-              ),
-            ),
-            SizedBox(height: 32,),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.8,
-              height: 50,
-              child: FilledButton(
-                style: FilledButton.styleFrom(backgroundColor: Colors.blueGrey[700]),
-                onPressed: () => context.push(routes.addMusicPath),
-                child: const Text(
-                  "Add Music",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                ),
-              ),
-            ),
-            SizedBox(height: 128,),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.8,
-              height: 70,
-              child: FilledButton(
-                style: FilledButton.styleFrom(backgroundColor: Colors.amber[900]),
-                onPressed: () {
-                  composeVM.saveRoutine(currentUser);
-                  context.push(routes.homePath);
-                },
-                child: const Text(
-                  "Create routine",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                ),
+            Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children:[
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    height: 50,
+                    child: FilledButton(
+                      style: FilledButton.styleFrom(backgroundColor: Colors.blueGrey[700]),
+                      onPressed: () => context.push(routes.addExercisePath),
+                      child: const Text(
+                        "Add Exercises",
+                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 32,),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    height: 50,
+                    child: FilledButton(
+                      style: FilledButton.styleFrom(backgroundColor: Colors.blueGrey[700]),
+                      onPressed: () => context.push(routes.addMusicPath),
+                      child: const Text(
+                        "Add Music",
+                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 128,),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    height: 70,
+                    child: FilledButton(
+                      style: FilledButton.styleFrom(backgroundColor: Colors.amber[900]),
+                      onPressed: () {
+                        composeVM.saveRoutine(currentUser);
+                        context.push(routes.homePath);
+                      },
+                      child: const Text(
+                        "Create routine",
+                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                      ),
+                    ),
+                    ),
+              ]
               ),
             ),
           ],
