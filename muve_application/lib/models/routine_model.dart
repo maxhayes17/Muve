5cf77b84beeff624675ce311c8010d425c79ddb5
jeremyB2 <99774165+jeremyB2@users.noreply.github.com>
2023-11-18 16:07:46 -0500

    saveRoutine to Firebase

--- muve_application/lib/models/routine_model.dart
+++ muve_application/lib/models/routine_model.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 import 'exercise_model.dart';
 import 'track_model.dart';
 
@@ -17,9 +19,59 @@ class Routine {
       required this.duration,
       required this.author,
       this.tags,
-      this.tracks,
-      this.exercises,
+      required this.tracks,
+      required this.exercises,
       this.picturePath});
+
+  factory Routine.fromFirestore(
+    DocumentSnapshot<Map<String, dynamic>> snapshot,
+    SnapshotOptions? options,
+  ) {
+    final data = snapshot.data();
+    return Routine(
+        id: data?['id'],
+        name: data?['name'],
+        duration: data?['duration'],
+        author: data?['author'],
+        // tags: data?['tags'],
+        tracks: data?['tracks'] is Iterable ? List.from(data?['tracks']) : null,
+        exercises: data?['exercises'] is Iterable
+            ? List.from(data?['exercises'])
+            : null,
+        picturePath: data?['picturePath']);
+  }
+
+  Map<String, dynamic> toFirestore() {
+    var exerciseList = List<Map<String, dynamic>>.empty(growable: true);
+    for (var e in exercises!) {
+      var exerciseMap = <String, dynamic>{};
+      exerciseMap['id'] = e.id;
+      exerciseMap['name'] = e.name;
+      // exerciseMap['notes'] = e.notes;
+      var setList = List<Map<String, dynamic>>.empty(growable: true);
+      for (var s in e.sets) {
+        var setMap = <String, dynamic>{};
+        setMap['id'] = s.id;
+        setMap['weight'] = s.weight;
+        setMap['repetitions'] = s.repetitions;
+        setMap['duration'] = s.duration;
+        setList.add(setMap);
+      }
+      exerciseMap['sets'] = setList;
+      exerciseList.add(exerciseMap);
+    }
+
+    return {
+      if (id != null) "id": id,
+      if (name != null) "name": name,
+      if (duration != null) "duration": duration,
+      if (author != null) "author": author,
+      if (tags != null) "tags": tags,
+      // if (tracks != null) "tracks": tracks,
+      if (exercises != null) "exercises": exerciseList,
+      if (picturePath != null) "picturePath": picturePath,
+    };
+  }
 
   factory Routine.fromJson(Map<String, dynamic> json) {
     return Routine(
--- muve_application/lib/viewmodels/compose_view_model.dart
+++ muve_application/lib/viewmodels/compose_view_model.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:muve_application/models/exercise_model.dart';
 import 'package:muve_application/models/routine_model.dart';
@@ -9,6 +10,7 @@ import 'package:muve_application/models/user_model.dart';
 import 'package:muve_application/data.dart';
 
 class ComposeViewModel with ChangeNotifier {
+  final db = FirebaseFirestore.instance;
 
   Routine? _newRoutine;
   List<Track> _trackSearchResults = [];
@@ -16,69 +18,88 @@ class ComposeViewModel with ChangeNotifier {
   List<Track>? get trackSearchResults => _trackSearchResults;
   Routine? get newRoutine => _newRoutine;
 
-  void createRoutine(User? user){
-    _newRoutine = Routine(        
-        id: numOfRoutines() + 1,
-        name: '',
-        duration: '00:00',
-        author: user!.username,
-        exercises: [Exercise(id: 1, sets: [ExerciseSet(id: 1)])],
-        tracks: [],
+  void createRoutine(User? user) {
+    _newRoutine = Routine(
+      id: numOfRoutines() + 1,
+      name: '',
+      duration: '00:00',
+      author: user!.username,
+      exercises: [
+        Exercise(id: 1, sets: [ExerciseSet(id: 1)])
+      ],
+      tracks: [],
     );
     // notifyListeners();
   }
 
   // Updating routine...
-  void updateRoutineName(String value){
+  void updateRoutineName(String value) {
     _newRoutine?.name = value;
     notifyListeners();
   }
-  void addExercise(){
-    _newRoutine?.exercises?.add(Exercise(id: _newRoutine!.exercises!.length + 1, sets: [ExerciseSet(id: 1)]));
+
+  void addExercise() {
+    _newRoutine?.exercises?.add(Exercise(
+        id: _newRoutine!.exercises!.length + 1, sets: [ExerciseSet(id: 1)]));
     notifyListeners();
   }
+
   void removeExercise(Exercise? exercise) {
     _newRoutine?.exercises?.remove(exercise);
     notifyListeners();
   }
 
-  void addTrack(Track? track){
+  void addTrack(Track? track) {
     _newRoutine?.tracks?.add(track!);
     notifyListeners();
   }
-      void removeTrack(Track? track) {
+
+  void removeTrack(Track? track) {
     _newRoutine?.tracks?.remove(track);
     notifyListeners();
   }
 
   // Updating exercise...
-  void addExerciseSet(Exercise? exercise){
+  void addExerciseSet(Exercise? exercise) {
     exercise?.sets?.add(ExerciseSet(id: exercise.sets!.length + 1));
     notifyListeners();
   }
-  void updateExerciseName(Exercise? exercise, String value){
+
+  void updateExerciseName(Exercise? exercise, String value) {
     exercise?.name = value;
     notifyListeners();
   }
 
-
   // Update exerciseSet...
-  void updateSetWeight(ExerciseSet? set, String value){
+  void updateSetWeight(ExerciseSet? set, String value) {
     set?.weight = int.parse(value);
     notifyListeners();
   }
-  void updateSetReps(ExerciseSet? set, String value){
+
+  void updateSetReps(ExerciseSet? set, String value) {
     set?.repetitions = int.parse(value);
     notifyListeners();
   }
-  void updateSetDuration(ExerciseSet? set, value){
+
+  void updateSetDuration(ExerciseSet? set, value) {
     set?.duration = value;
     notifyListeners();
   }
 
-  void saveRoutine(User? user) {
+  void saveRoutine(User? user) async {
     routines.add(_newRoutine!);
     user?.routines?.add(_newRoutine!);
+
+    //add to Firestore db
+    final docRef = db
+        .collection("routines")
+        .withConverter(
+          fromFirestore: Routine.fromFirestore,
+          toFirestore: (Routine routine, options) => routine.toFirestore(),
+        )
+        .doc();
+    await docRef.set(_newRoutine!);
+
     // Reset _newRoutine
     createRoutine(user);
   }
@@ -87,19 +108,14 @@ class ComposeViewModel with ChangeNotifier {
     return routines.length;
   }
 
-  void searchTracks(String value){
+  void searchTracks(String value) {
     List<Track> results = [];
-    for (var track in tracks){
-      if (track.name.contains(value)){
+    for (var track in tracks) {
+      if (track.name.contains(value)) {
         results.add(track);
       }
     }
     _trackSearchResults = results;
     notifyListeners();
   }
-
-
-
 }
-
-
